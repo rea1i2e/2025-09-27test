f6a93f64e9c24549862cca796ad0acb132e7ce6a
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-10-24 11:55:07 +0900

    不要画像削除／デプロイ後相対パス

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 export default defineConfig({
-  base: '/2025-09-27test/',
+  base: './', // ビルド後は相対パスへ
   build: {
     outDir: 'docs', // GitHub Pagesで公開できるのは`/`or`docs/` のみ
   },
   server: {
-    open: true,
+    open: true, // ブラウザ自動起動
   },
 })
