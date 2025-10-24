201c70cf287e705dd7308d18a4449c56b1f9bd13
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-10-24 10:46:15 +0900

    ビルド後のパス設定

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 export default defineConfig({
-  base: './',
+  base: '/2025-09-27test/',
   build: {
-    outDir: 'docs',
+    outDir: 'docs', // GitHub Pagesで公開できるのは`/`or`docs/` のみ
   },
   server: {
     open: true,
